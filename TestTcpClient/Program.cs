170337739188b6aced0aff1707717b1cf803ddfc
hironobu koura <hironobu@meuzz.cc>
2022-08-02 19:44:02 +0900

    TestTcpClient/TestTcpServer added

---
+++ TestTcpClient/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using Meuzz.Linq.Serialization;
+
+namespace TcpClient
+{
+    class Client
+    {
+        private static void Main(String[] args)
+        {
+            Thread.Sleep(5000);
+
+            IPHostEntry iphostInfo = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = iphostInfo.AddressList[0];
+            IPEndPoint ipEndpoint = new IPEndPoint(ipAddress, 9999);
+
+            Socket client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                client.Connect(ipEndpoint);
+
+                Console.WriteLine("Socket created to {0}", client.RemoteEndPoint.ToString());
+
+                // byte[] sendmsg = Encoding.UTF8.GetBytes("This is from Client\n");
+                var ss = "hogehoge";
+                var s = JsonNetSerializer.Serialize<Func<string, bool>>(x => x == ss);
+                var data = Encoding.UTF8.GetBytes((string)s);
+
+                int n = client.Send(data);
+
+                var buf = new byte[1024 * 64];
+                int m = client.Receive(buf);
+
+                Console.WriteLine("" + Encoding.ASCII.GetString(data));
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("Transmission end.");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TestTcpServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+using Meuzz.Linq.Serialization;
+using System.Diagnostics;
+
+namespace ServerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.IPv6Any, 9999);
+
+            server.Start();
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+
+                NetworkStream ns = client.GetStream();
+
+                byte[] hello = new byte[100];
+                hello = Encoding.Default.GetBytes("hello world");
+
+                ns.Write(hello, 0, hello.Length);
+
+                while (client.Connected)
+                {
+                    byte[] msg = new byte[1024 * 64];
+                    ns.Read(msg, 0, msg.Length);
+
+                    var s = Encoding.UTF8.GetString(msg);
+
+                    var ff = JsonNetSerializer.Deserialize<Func<string, bool>>(s);
+
+                    Debug.WriteLine(ff("hogehoge"));
+                    Debug.WriteLine(s);
+                }
+            }
+
+        }
+    }
+}
